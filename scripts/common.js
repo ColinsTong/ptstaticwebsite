e162b49a9846aa2ea832ec9fb4f3fa1d9d0ceb55
Fukumakoto Kure <kure.fukumakoto@gmail.com>
2015-08-20 22:29:15 +0800

    fix bug

--- scripts/common.js
+++ scripts/common.js
@@ -1,4 +1,4 @@
-function scrollToCenter(el) {
+function scrollToCenter(el,delay,delayExecute) {
     var windowHeight = $(window).height();
     var elObj = $(el);
     var elCenterOffset = Math.round(elObj.offset().top + elObj.height()/2);
@@ -6,8 +6,8 @@ function scrollToCenter(el) {
     var scrollValue = Math.round(elCenterOffset - windowHeight / 2);
     if (windowHeight < elBottomOffset && $(document).scrollTop() < scrollValue) {
         setTimeout(function() {
-            $.scrollTo(scrollValue, 2000)
-        }, 500);
+            $.scrollTo(scrollValue, delay)
+        }, delayExecute);
     };
 }
 
