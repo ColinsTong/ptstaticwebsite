fe5ef4e189086840c6760c5d2adf8ee0130cc775
Fukumakoto Kure <kure.fukumakoto@gmail.com>
2015-09-05 17:59:28 +0800

    add picdisply func

---
+++ scripts/picdisplay.js
@@ -0,0 +1,24 @@
+$(window).load(function () {
+    $('.picunit a').each(function (index, el) {
+        $(el).click(function (event) {
+            //document.documentElement.style.overflow = 'hidden';
+            $('.picdetail').css('width', $(window).width())
+            $('.picdetail').css('height', $(window).height())
+            $('.picdetail').show(500);
+            event.preventDefault();
+        });
+    });
+    $('.picdetail').click(function (event) {
+        $('.picdetail').hide(500);
+        //document.documentElement.style.overflow = 'auto';
+        event.preventDefault();
+    });
+    $('.picarticle').click(function (event) {
+        event.stopPropagation();
+    });
+    $('.close').click(function (event) {
+        $('.picdetail').hide(500);
+        //document.documentElement.style.overflow = 'auto';
+        event.preventDefault();
+    });
+});
