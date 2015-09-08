55c459e86b28fb7f2b42e113492570e65aae7b83
Fukumakoto Kure <kure.fukumakoto@gmail.com>
2015-09-09 00:33:07 +0800

    finish girlledisplay

---
+++ scripts/grilledisplay.js
@@ -0,0 +1,54 @@
+var page = 10;
+
+$(window).load(function () {
+    $('.large').each(function (index, el) {
+        $(el).click(function (event) {
+            //document.documentElement.style.overflow = 'hidden';
+            $('.picdetail').css('width', $(window).width())
+            $('.picdetail').css('height', $(window).height())
+            $('.picdetail').show(500);
+            event.preventDefault();
+        });
+    });
+    $('.picdetail').click(function (event) {
+        $('.picdetail').hide(500);
+        //document.documentElement.style.overflow = 'auto';
+        event.preventDefault();
+    });
+    $('.picarticle').click(function (event) {
+        event.stopPropagation();
+    });
+    $('.close').click(function (event) {
+        $('.picdetail').hide(500);
+        //document.documentElement.style.overflow = 'auto';
+        event.preventDefault();
+    });
+    $('.next').click(function (event) {
+        page += 1
+        $('.small').each(function (index, el) {
+            $(el).css('background-image', 'url(../images/grilledisplay/' + ((page + 1 + index) % 11 + 1) + '.0.jpg)');
+            // console.log(el);
+        });
+        event.preventDefault();
+    });
+    $('.prev').click(function (event) {
+        if (page < 0) {
+            page = 10;
+        }
+        page -= 1;
+        $('.small').each(function (index, el) {
+            $(el).css('background-image', 'url(../images/grilledisplay/' + ((page + 1 + index) % 11 + 1) + '.0.jpg)');
+            // console.log(el);
+        });
+        event.preventDefault();
+    });
+    $('.small').each(function (index, el) {
+        $(el).click(function (event) {
+            $('.large').css('background-image', $(el).css('background-image'));
+            event.preventDefault();
+        });
+    });
+
+
+
+});
